8f4c2b8cad5797699b36f69051d4363d31f15338
YK <yoshihiko.kakutani@gmail.com>
2019-11-17 18:48:12 +0900

    Support modulo operations in Haskell

--- main.hs
+++ main.hs
@@ -34,7 +34,7 @@ fib n = foldlHist step [fib 1, fib 0] [2..n]
     where step a@(a0:a1:_) n = (a0 + a1) : a
 
 foldlState :: (c -> a -> b -> (c, a)) -> c -> a -> [b] -> a
-foldlState f s a l = snd $ foldl (\(s, a) -> f s a) (s, a) l
+foldlState f s a l = snd $ foldl (\(s,a) -> f s a) (s, a) l
 
 foldlUntil :: (b -> Bool) -> (a -> b -> a) -> a -> [b] -> a
 foldlUntil p f a [] = a
@@ -64,4 +64,38 @@ myCompare (ax:bx:_) (ay:by:_)
     | otherwise = EQ
 myCompare _ _ = EQ
 
+-- mod
+
+addMod :: Int -> Int -> Int -> Int
+addMod p x y = mod (x + y) p
+
+mulMod :: Int -> Int -> Int -> Int
+mulMod p x y = mod (x * y) p
+
+invMod :: Int -> Int -> Int
+invMod p x = mod (xgcd p x !! 2) p
+
+xgcd :: Int -> Int -> [Int]
+xgcd a b |     a < b = xgcdRec [0, 1, 1, 0] b a
+         | otherwise = xgcdRec [1, 0, 0, 1] a b
+         where xgcdRec m a 0 = a : m
+               xgcdRec [x0,y0,x1,y1] a b = let d = div a b in xgcdRec [x1, y1, x0 - d * x1, y0 - d * y1] b (mod a b)
+
+factMod :: Int -> Int -> Int
+factMod p 0 = 1
+factMod p n = mulMod p n $ factMod p (n-1)
+
+cmbMod :: Int -> Int -> Int -> Int
+cmbMod p n r = mulMod p (foldl (mulMod p) 1 [n-r+1..n]) (foldl (mulMod p) 1 $ map (invMod p) [1..r])
+
 -- EOF
+
+-- coding rules
+
+-- ### no spacing
+-- * pattern
+-- * index
+-- * easy arithmetic
+
+-- ### additional spacing
+-- * list comprehension
